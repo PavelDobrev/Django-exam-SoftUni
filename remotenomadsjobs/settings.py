9a965be36cffabfb9107885c946ffa1c67c7d150
Pavel <pavelxragomirovdobrev@gmail.com>
2023-08-18 17:22:14 +0300

    last

--- remotenomadsjobs/settings.py
+++ remotenomadsjobs/settings.py
@@ -15,17 +15,38 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+
+
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-w9v8f_f@!e3&301(naoc47#d1*6&h5w*73w_$^4wlwm%2m+$tl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS=['127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS= os.environ.get('ALLOWED_HOSTS', '').split(' ')
+DATABASES = {
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.getenv('DB_NAME', None),
+
+        'USER': os.getenv('DB_USER', None),
+
+        'PASSWORD': os.getenv('DB_PASS', None),
+
+        'HOST': os.getenv('DB_HOST', None),
+
+        'PORT': os.getenv('DB_PORT', '5432'),
+
+    }
+
+}
+CSRF_TRUSTED_ORIGINS = [f'http://{x}:81' for x in os.environ.get('ALLOWED_HOSTS', '').split(' ') ]
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,25 +97,7 @@ WSGI_APPLICATION = 'remotenomadsjobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
 
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'postgres',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'postgres',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
-    }
-
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -142,5 +145,8 @@ MEDIA_ROOT = 'remotenomadsjobs/media/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = (
+    BASE_DIR / '/static_files/'
+)
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
